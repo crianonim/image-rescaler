b218aefc0242923c1886313bbbc5f3cc3e23b9a7
Jan Skowronski <crianonim@gmail.com>
2019-12-29 17:16:13 +0000

    stringify

--- public/javascripts/tracker.js
+++ public/javascripts/tracker.js
@@ -3,4 +3,4 @@ localStorage.setItem('zd_tracker_id',id);
 const title=(document.querySelector('meta[property*="og:title"]')||{content:'not article'}).content
 console.log(`User id: ${id} visited page:${title}`)
 const timestamp=Date.now()
-fetch("https://permutator.jans.site/api/zdtracker?"+timestamp,{method:"POST",body:{id,title,timestamp}}).then(console.log)
+fetch("https://permutator.jans.site/api/zdtracker?"+timestamp,{method:"POST",body:JSON.stringify({id,title,timestamp})}).then(console.log)
--- routes/api/zdtracker.js
+++ routes/api/zdtracker.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 router.post('/',(req,res)=>{
     console.log("BODY",req.body);
-    res.send("ok fetch");
+    res.send("ok fetch"+JSON.stringify(req.body));
 })
 
 module.exports = router;
