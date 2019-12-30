de1acf096e4a4f95b2d02c8123a396a1ec7abf51
Jan Skowronski <crianonim@gmail.com>
2019-12-30 08:30:41 +0000

    add sqlite3

--- routes/api/zdtracker.js
+++ routes/api/zdtracker.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('tracker.db');
+db.run("CREATE TABLE IF NOT EXISTS pageview_events (id INTEGER PRIMARY KEY, user_id TEXT NOT NULL, timestamp INTEGER NOT NULL, title TEX)");
 router.post('/',(req,res)=>{
     console.log("BODY",req.body);
+    const {id,title,timestamp}=req.body;
+    db.run(`INSERT INTO pageview_events(user_id,timestamp,title) VALUES ('${id}',${timestamp},'${title}')`)
     res.json(req.body);
 })
 
