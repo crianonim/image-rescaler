fdc4b764099312c3ec9a5c0a16ae389a04c1eb55
Jan Skowronski <crianonim@gmail.com>
2019-12-30 10:43:34 +0000

    change address url

--- public/javascripts/tracker.js
+++ public/javascripts/tracker.js
@@ -6,7 +6,8 @@ localStorage.setItem('zd_tracker_id',id);
 const title=(document.querySelector('meta[property*="og:title"]')||{content:'not article'}).content
 console.log(`User id: ${id} visited page:${title}`)
 const timestamp=Date.now()
-fetch("/api/zdtracker?"+timestamp,{method:"POST",headers: {
+const url="https://permutator.jans.site"
+fetch(url+"/api/zdtracker?"+timestamp,{method:"POST",headers: {
     'Content-Type': 'application/json'
     
   },body:JSON.stringify({id,title,timestamp})}).then(res=>res.json()).then(console.log)
