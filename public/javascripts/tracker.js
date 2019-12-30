4f9debd021030cc45b2e30d2a9539253a60b3ff5
Jan Skowronski <crianonim@gmail.com>
2019-12-30 08:18:30 +0000

    set demo on localhost

--- public/javascripts/tracker.js
+++ public/javascripts/tracker.js
@@ -3,7 +3,7 @@ localStorage.setItem('zd_tracker_id',id);
 const title=(document.querySelector('meta[property*="og:title"]')||{content:'not article'}).content
 console.log(`User id: ${id} visited page:${title}`)
 const timestamp=Date.now()
-fetch("https://permutator.jans.site/api/zdtracker?"+timestamp,{method:"POST",headers: {
+fetch("/api/zdtracker?"+timestamp,{method:"POST",headers: {
     'Content-Type': 'application/json'
     
-  },body:JSON.stringify({id,title,timestamp})}).then(console.log)
+  },body:JSON.stringify({id,title,timestamp})}).then(res=>res.json()).then(console.log)
